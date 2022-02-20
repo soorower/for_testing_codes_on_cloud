e51b56b06574b04f8225962d4b872688bc6ccb4b
soorower <sorowerhossan01@gmail.com>
2022-02-20 08:54:35 +0600

    make it better

--- greenweez.py
+++ greenweez.py
@@ -12,7 +12,7 @@ headers = {
 
 
 def scrape_subcategory_links():
-    all_links = pd.read_excel('greenweez_categories.xlsx')['links'].tolist() # collecting all category links from input excel(greenweez_categories.xlsx)
+    all_links = pd.read_excel('greenweez_categories1.xlsx')['links'].tolist() # collecting all category links from input excel(greenweez_categories.xlsx)
 
     data1 = {}
     lists1 = []
@@ -85,7 +85,7 @@ def scrape_links_from_each_subcategory(): # collecting product links from each s
         except:
             pass
     df3 = pd.DataFrame(lists2).drop_duplicates(subset=['Product link']) # removing duplicates from links..
-    df3.to_excel('products_links.xlsx',encoding = 'utf-8-sig',index = False) # saving the data sheet with all links to an excel
+    df3.to_excel('products_links2.xlsx',encoding = 'utf-8-sig',index = False) # saving the data sheet with all links to an excel
 
 
 if __name__ == '__main__':    
@@ -93,8 +93,8 @@ if __name__ == '__main__':
  
 
 embed = DiscordEmbed(title='greenweez', description=f'''Urls Yesterday:''')
-with open(f'products_links.xlsx', "rb") as f:
-    webhook.add_file(file=f.read(), filename=f'products_links.xlsx')
+with open(f'products_links2.xlsx', "rb") as f:
+    webhook.add_file(file=f.read(), filename=f'products_links2.xlsx')
 webhook.add_embed(embed)
 response = webhook.execute()
 webhook.remove_embeds()
